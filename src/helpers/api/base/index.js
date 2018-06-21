d3512ae7ad9777284257c1283a3b7dd55288a81f
JRuan Herculano <ruannawe95@gmail.com>
2018-06-21 19:41:53 -0300

    [ FIX ] Desabilitando lint em pontos específicos

--- src/helpers/api/base/index.js
+++ src/helpers/api/base/index.js
@@ -1,7 +1,8 @@
 import instance from './instance';
 
 const defaultConfig = () => ({
-  headers: Object.assign({}, { Cookie: 'PHPSESSID=f35d28dcf98f13fded03d151ae088664' }),
+  // eslint-disable-next-line
+  headers: Object.assign({}, { 'Cookie': 'PHPSESSID=f35d28dcf98f13fded03d151ae088664' }),
 });
 
 export default class API {
