483ea1e0369ec6063a1d5522efd5db4f602e2d20
JRuan Herculano <ruannawe95@gmail.com>
2018-06-21 18:16:10 -0300

    [ ADD ] Buscando informações de projetos

--- src/components/PrestacaoDeContas/ListComprovantes.vue
+++ src/components/PrestacaoDeContas/ListComprovantes.vue
@@ -1,8 +1,31 @@
 <template>
   <div>
-    <div v-for="comprovante in comprovantes" v-bind:key="comprovante.id" class="">
-      <p>{{comprovante}}</p>
-      <br>
+    <div v-for="projeto in comprovantes" v-bind:key="projeto.id" class="">
+      <div class="card">
+        <div class="card-content">
+          <span class="card-title">{{projeto.NomeProjeto}}</span>
+          <table class="tabela">
+            <tbody>
+              <tr>
+                <th>Produto:</th>
+                <td>{{projeto.Produto}}</td>
+                <th>Etapa:</th>
+                <td>{{projeto.Etapa}}</td>
+                <th>Item de Custo:</th>
+                <td>{{projeto.Item}}</td>
+              </tr>
+              <tr>
+                <th>Valor Aprovado:</th>
+                <td>R$ {{ formatValues(projeto.vlAprovado) }}</td>
+                <th>Valor Comprovado:</th>
+                <td>R$ {{ formatValues(projeto.vlComprovado) }}</td>
+                <th>Comprovação Validada:</th>
+                <td>R$ {{ formatValues(projeto.ComprovacaoValidada) }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +42,19 @@ export default {
     ...mapActions({
       fetchComprovantes: 'prestacaoDeContas/default',
     }),
+    formatValues: function (value) {
+        if (value === undefined) {
+            return '0';
+        }
+
+        if (value.indexOf('.') === -1) {
+            value = value.concat('.00');
+        }
+
+        const valueChangedPointByComma = value.replace('.', ',');
+
+        return valueChangedPointByComma.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
   },
   computed: {
     ...mapGetters({
