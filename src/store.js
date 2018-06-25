52d2a7914b531f41cfb93adbf55322b1d231b8fe
JRuan Herculano <ruannawe95@gmail.com>
2018-06-25 15:23:51 -0300

    [ ADD ] Testa dependias de bue bootstrap e vuetify

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import BootstrapVue from 'bootstrap-vue';
 import Vuetify from 'vuetify';
 
 import prestacaoDeContas from './components/PrestacaoDeContas/store';
 
-Vue.use(Vuex, Vuetify);
+Vue.use(Vuex);
+Vue.use(BootstrapVue);
+Vue.use(Vuetify);
 
 const debug = process.env.NODE_ENV !== 'production' || process.env.NODE_ENV !== 'staging';
 
