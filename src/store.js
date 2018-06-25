44819ac461420584566614d2e54eb6bd1f8a45ac
JRuan Herculano <ruannawe95@gmail.com>
2018-06-25 14:28:52 -0300

    [ ADD ] Importa vuetifiy para a posta store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Vuetify from 'vuetify';
 
 import prestacaoDeContas from './components/PrestacaoDeContas/store';
 
-Vue.use(Vuex);
+Vue.use(Vuex, Vuetify);
 
 const debug = process.env.NODE_ENV !== 'production' || process.env.NODE_ENV !== 'staging';
 
