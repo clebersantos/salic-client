a383ee1b57070f3e37dfafe2b76db3aab838c045
JRuan Herculano <ruannawe95@gmail.com>
2018-06-26 17:50:09 -0300

    [ ADD ] Adiciona configuração para que o jest não teste os node modules

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -15,7 +15,8 @@ module.exports = {
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   testPathIgnorePatterns: [
-    '<rootDir>/test/e2e'
+    '<rootDir>/test/e2e',
+    '<rootDir>/node_modules'
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
