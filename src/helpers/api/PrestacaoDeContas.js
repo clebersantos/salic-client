ac0685b2f3297279c41fc51992dd75bb4faa381a
JRuan Herculano <ruannawe95@gmail.com>
2018-06-29 11:07:49 -0300

    [ ADD ] instancia da classe do axios

--- src/helpers/api/PrestacaoDeContas.js
+++ src/helpers/api/PrestacaoDeContas.js
@@ -1,6 +1,6 @@
 import API from './base';
 
-const api = () => new API();     
+const api = () => new API();
 
 export const buscaProjeto = () => {
   const url = 'http://172.20.0.3/prestacao-contas/planilha-aprovacao/get?idPronac=132451&idPlanilhaItem=130&produto=0&uf=SP&idmunicipio=355030';
